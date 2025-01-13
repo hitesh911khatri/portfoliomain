b0d9d31a34f9e4c06506ac5bdca8c01cbb12e266
hitesh911khatri <149157855+hitesh911khatri@users.noreply.github.com>
2025-01-13 19:38:46 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item>
               <Nav.Link
-                href="https://soumyajitblogs.vercel.app/"
+                href="https://hitesh911khatri.vercel.app/"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/hitesh911khatri/Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
